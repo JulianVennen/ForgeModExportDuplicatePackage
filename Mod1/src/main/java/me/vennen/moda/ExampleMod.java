fdfe2d1fa38609f8d95625a7e34cb1beb85abe64
Julian <julian@aternos.org>
2023-02-08 17:30:53 +0100

    create two mods that export the same package

---
+++ Mod1/src/main/java/me/vennen/moda/ExampleMod.java
@@ -0,0 +1,29 @@
+package me.vennen.moda;
+
+import com.mojang.logging.LogUtils;
+import me.vennen.common.CommonClass;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod
+{
+    public static final String MODID = "testmoda";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod()
+    {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        CommonClass.printSomething(LOGGER);
+    }
+}
---
+++ Mod2/src/main/java/me/vennen/common/CommonClass.java
@@ -0,0 +1,9 @@
+package me.vennen.common;
+
+import org.slf4j.Logger;
+
+public class CommonClass {
+    public static void printSomething(Logger logger) {
+        logger.info("something");
+    }
+}
